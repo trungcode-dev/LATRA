dce7fdfdb739ecfaf0f311718a46f796b8f0cca4
anhduc <nguyenanhduc2007b@gmail.com>
2026-04-10 20:42:45 +0700

    Xong UI Tra cuu bao hanh va Form Thanh toan

---
+++ Latra-Fornend/latra-fornend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+// Đây là file gốc của toàn bộ ứng dụng
+</script>
+
+<style>
+/* Reset một chút CSS cơ bản cho web đỡ bị lệch */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f4f7f6;
+}
+</style>
--- Latra-Fornend/latra-fornend/src/main.ts
+++ Latra-Fornend/latra-fornend/src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' // 1. Nhập cái dây cáp Router vào
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router) // 2. Cắm phích điện! (Báo cho Vue biết là sẽ dùng Router)
+
+app.mount('#app')
