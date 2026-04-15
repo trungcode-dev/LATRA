2ac6e1a08806388a40bb6a576e7cad11e0528eca
trungcode-dev <trungly498@gmail.com>
2026-04-15 21:32:43 +0700

    nguyen-ban

--- Latra-Fornend/latra-fornend/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // 1. Nhập cái dây cáp Router vào
-
-const app = createApp(App)
-
-app.use(router) // 2. Cắm phích điện! (Báo cho Vue biết là sẽ dùng Router)
-
-app.mount('#app')
--- latra-admin/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-// Lưu ý: Đã xóa dòng import style.css của bước Tailwind cũ đi
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
