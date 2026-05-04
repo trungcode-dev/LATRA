1ca4ae756d85426ef7ffd9e91df4e219b09befd8
huynd-developer <nguyhuy1408@gmail.com>
2026-05-04 18:26:33 +0700

    "Hòan thiện 90% chức năng bên quản lý"

--- LATRA/src/main/java/trungdevcode/latra/Entity/Order.java
+++ LATRA/src/main/java/trungdevcode/latra/Entity/Order.java
@@ -1,6 +1,5 @@
 package trungdevcode.latra.Entity;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
 import java.math.BigDecimal;
@@ -19,10 +18,22 @@ public class Order {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne
+    @JoinColumn(name = "employee_id")
+    private User employee;
+
     private BigDecimal totalAmount;
     private String status;
     private LocalDateTime createdAt;
 
+    // ==========================================
+    // THÊM 3 CỘT ĐỂ XỬ LÝ THANH TOÁN KẾT HỢP
+    // ==========================================
+    private String paymentMethod; // "CASH" (Tiền mặt), "TRANSFER" (Chuyển khoản), "SPLIT" (Kết hợp)
+    private BigDecimal cashAmount;     // Số tiền mặt khách đưa
+    private BigDecimal transferAmount; // Số tiền khách chuyển khoản
+    @Column(columnDefinition = "NVARCHAR(MAX)")
+    private String note;
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Shipments shipment;
 
